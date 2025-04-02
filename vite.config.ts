6043ec57a997e906f47459b5fb1ca8a3d22a4975
Niko Salonen <nikotsalonen@gmail.com>
2025-04-02 12:53:12 +0300

    Update Vite configuration with base path and build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/viikko-info/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
